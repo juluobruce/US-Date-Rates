29359b53d373ac779e44fcf408b4fa0efe321f21
juluobruce <42151923+juluobruce@users.noreply.github.com>
2019-06-15 23:01:17 -0700

    updated

---
+++ deathrates.sql
@@ -0,0 +1,16 @@
+-- Create a new table
+DROP TABLE Deathrates;
+
+CREATE TABLE Deathrates (
+  "Year" INT,
+  "113 Cause Name" VARCHAR(250),
+  "Cause Name" VARCHAR(250),
+  "State" VARCHAR(250),
+  "Deaths" INT,
+  "Age_adjusted Death Rate" FLOAT
+);
+
+-- view the query 
+
+SELECT * FROM Deathrates;
+
