8576bfac81d7ad1de345e7852fe81bb25bbf78ad
juluobruce <42151923+juluobruce@users.noreply.github.com>
2019-06-23 15:02:25 -0700

    update

--- deathrates.sql
+++ deathrates.sql
@@ -15,5 +15,43 @@ CREATE TABLE Deathrates (
 SELECT * FROM Deathrates;
 
 
--- seperate by Cause_Name 
+-- seperate by State (CA) -- descending order by year 
+-- to see the diff diseases in CA
+
+SELECT 
+"Cause_Name",
+"State",
+"Year"
+FROM Deathrates
+WHERE "State" = 'California'
+ORDER BY "Year" DESC
+;
+
+-- Specify by Cause_Name (Suicide) by state (California) per year
+SELECT
+"Cause_Name",
+"State",
+"Deaths",
+"Year"
+FROM Deathrates
+WHERE "Cause_Name" = 'Suicide' AND "State" = 'California'
+ORDER BY "Year" DESC
+;
+
+-- Exporting as CSV file 
+-- There's an option of doing it via postgres
+
+\copy (SELECT * FROM persons) to 'C:\tmp\persons_client.csv' with csv
+
+COPY (SELECT
+"Cause_Name",
+"State",
+"Deaths",
+"Year"
+FROM Deathrates
+WHERE "Cause_Name" = 'Suicide' AND "State" = 'California'
+ORDER BY "Year" DESC)
+TO 'C:\tmp\suicide_rate_CA.csv' WITH CSV
+;
+
 
